4afebc10a6920260fb6751eb61101973f90ebf07
Artur Wiśniewski <artixbtw@gmail.com>
2023-10-06 13:50:50 +0200

    delete ProductToBuy.jsx and add files from her to SingleProduct.jsx

--- src/pages/SingleProduct.jsx
+++ src/pages/SingleProduct.jsx
@@ -1,10 +1,24 @@
 import { useParams } from "react-router-dom";
-import { ProductToBuy } from "../components/ProductToBuy";
 import productsData from "../data.json";
+import { MainInfoProduct } from "../components/MainInfoProduct";
+import { FeaturesInfoProduct } from "../components/FeaturesInfoProduct";
+import { PhotosProduct } from "../components/PhotosProduct";
+import { SuggestProducts } from "../components/SuggestProducts";
+import { ProductsCategory } from "../components/ProductsCategory";
+import { AboutUs } from "../components/AboutUs";
 
 export const SingleProduct = () => {
   const { slug } = useParams();
 
   const singleProduct = productsData.find((item) => item.slug === slug);
-  return <ProductToBuy item={singleProduct} />;
+  return (
+    <>
+      <MainInfoProduct item={singleProduct} />
+      <FeaturesInfoProduct item={singleProduct} />
+      <PhotosProduct item={singleProduct} />
+      <SuggestProducts item={singleProduct} />
+      <ProductsCategory />
+      <AboutUs />
+    </>
+  );
 };
