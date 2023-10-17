6c500d619ec3d2a58dff378c667f4dd2f0d24da1
Artur Wiśniewski <artixbtw@gmail.com>
2023-10-17 21:40:53 +0200

    add radioButton elements

--- src/components/PaymentDetails.jsx
+++ src/components/PaymentDetails.jsx
@@ -1,10 +1,44 @@
+import { useState } from "react";
+
 export const PaymentDetails = () => {
-  const inputStyles = `rounded-lg border border-[#CFCFCF] px-4 py-3 text-sm font-bold outline-[#D87D4A]`;
+  // const inputStyles = `rounded-lg border border-[#CFCFCF] px-4 py-3 text-sm font-bold outline-[#D87D4A]`;
+
+  const [selectedPayment, setSelectedPayment] = useState("eMoney");
+  const handleOptionChange = (event) => {
+    setSelectedPayment(event.target.value);
+  };
+
   return (
     <div>
       <h3 className="mb-4 mt-8 font-bold text-[#D87D4A]">PAYMENT DETAILS</h3>
-      <div className="grid grid-cols-2 gap-8">
-        <form className="col-span-full flex flex-col">
+      <div className="grid grid-cols-2 gap-8 ">
+        <form className="flex flex-col">
+          <label className="my-2 rounded-lg border border-[#CFCFCF] px-4 py-3">
+            <input
+              type="radio"
+              value="eMoney"
+              checked={selectedPayment === "eMoney"}
+              onChange={handleOptionChange}
+            />
+            <span className="px-6 py-4 font-bold">e-Money</span>
+          </label>
+          <label>
+            <input
+              type="radio"
+              value="Cash"
+              checked={selectedPayment === "Cash"}
+              onChange={handleOptionChange}
+            />
+            <span>Cash on Delivery</span>
+          </label>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+{
+  /* <form className="col-span-full flex flex-col">
           <span className="mb-1 text-sm font-bold text-black">Address</span>
           <input
             type="text"
@@ -15,8 +49,5 @@ export const PaymentDetails = () => {
         <form className="flex flex-col">
           <span className="mb-1 text-sm font-bold text-black">ZIP Code</span>
           <input type="text" placeholder="120091" className={inputStyles} />
-        </form>
-      </div>
-    </div>
-  );
-};
+        </form> */
+}
