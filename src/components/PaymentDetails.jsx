a9d01fa174db3919bf04d4d512507482e33eda88
Artur Wiśniewski <artixbtw@gmail.com>
2023-10-17 21:59:06 +0200

    add styles to radioButtons

--- src/components/PaymentDetails.jsx
+++ src/components/PaymentDetails.jsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
 
 export const PaymentDetails = () => {
-  // const inputStyles = `rounded-lg border border-[#CFCFCF] px-4 py-3 text-sm font-bold outline-[#D87D4A]`;
+  const inputStyles = `rounded-lg border border-[#CFCFCF] px-4 py-3 text-sm font-bold outline-[#D87D4A]`;
+  const labelStyles = `my-2 rounded-lg border border-[#CFCFCF] px-4 py-3 hover:border-[#D87D4A] cursor-pointer`;
 
   const [selectedPayment, setSelectedPayment] = useState("eMoney");
   const handleOptionChange = (event) => {
@@ -12,33 +13,31 @@ export const PaymentDetails = () => {
     <div>
       <h3 className="mb-4 mt-8 font-bold text-[#D87D4A]">PAYMENT DETAILS</h3>
       <div className="grid grid-cols-2 gap-8 ">
+        <div>
+          <span className="text-sm font-bold">Payment Method</span>
+        </div>
         <form className="flex flex-col">
-          <label className="my-2 rounded-lg border border-[#CFCFCF] px-4 py-3">
+          <label className={labelStyles}>
             <input
               type="radio"
               value="eMoney"
               checked={selectedPayment === "eMoney"}
               onChange={handleOptionChange}
+              className="  text-orange-600 "
             />
             <span className="px-6 py-4 font-bold">e-Money</span>
           </label>
-          <label>
+          <label className={labelStyles}>
             <input
               type="radio"
               value="Cash"
               checked={selectedPayment === "Cash"}
               onChange={handleOptionChange}
             />
-            <span>Cash on Delivery</span>
+            <span className="px-6 py-4 font-bold">Cash on Delivery</span>
           </label>
         </form>
-      </div>
-    </div>
-  );
-};
-
-{
-  /* <form className="col-span-full flex flex-col">
+        <form className="flex flex-col">
           <span className="mb-1 text-sm font-bold text-black">Address</span>
           <input
             type="text"
@@ -49,5 +48,13 @@ export const PaymentDetails = () => {
         <form className="flex flex-col">
           <span className="mb-1 text-sm font-bold text-black">ZIP Code</span>
           <input type="text" placeholder="120091" className={inputStyles} />
-        </form> */
-}
+        </form>
+        <p>
+          The ‘Cash on Delivery’ option enables you to pay in cash when our
+          delivery courier arrives at your residence. Just make sure your
+          address is correct so that your order will not be cancelled.
+        </p>
+      </div>
+    </div>
+  );
+};
