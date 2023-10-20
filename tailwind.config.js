8c7311ffb8aac39dbbe15e92c0cfe9763a48ec98
Artur Wiśniewski <artixbtw@gmail.com>
2023-10-20 14:20:20 +0200

    relocation bg image to public folder

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ export default {
   theme: {
     extend: {
       backgroundImage: {
-        hero: "url('src/assets/image-hero.jpg')",
+        hero: "url('../public/assets/home/desktop/image-hero.jpg')",
       },
     },
   },
