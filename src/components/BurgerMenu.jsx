617cdcb16f6ed33f6f3290cb0841079536d0b290
Artur Wiśniewski <artixbtw@gmail.com>
2023-10-21 12:48:55 +0200

    add logic to burgerMenu.jsx

--- src/components/BurgerMenu.jsx
+++ src/components/BurgerMenu.jsx
@@ -1,18 +1,41 @@
+import { useState } from "react";
 import { Navigation } from "../components/Navigation";
 
 export const BurgerMenu = () => {
+  const [showBurgerMenu, setShowBurgerMenu] = useState(false);
+
+  if (showBurgerMenu === true) {
+    document.body.style.overflow = "hidden";
+  } else {
+    document.body.style.overflow = "visible";
+  }
+
   return (
     <div className="hidden cursor-pointer max-md:order-first max-md:block">
-      <div className="relative h-[3px] w-6 bg-white before:absolute before:bottom-2 before:block before:h-full before:w-full before:bg-white before:content-[''] after:absolute after:top-2 after:block after:h-full after:w-full after:bg-white after:content-['']  "></div>
-      <div className="absolute  left-0 top-0 h-full w-full overflow-hidden bg-black bg-opacity-20">
-        <div className="h-full w-5/12 bg-[#1a1919]">
-          <Navigation
-            styleClassFlex={
-              "flex-col flex space-x-0 space-y-20 text-xl items-center pt-7"
-            }
-          />
+      <div
+        onClick={() => setShowBurgerMenu(!showBurgerMenu)}
+        className="relative h-[3px] w-6 bg-white before:absolute before:bottom-2 before:block before:h-full before:w-full before:bg-white before:content-[''] after:absolute after:top-2 after:block after:h-full after:w-full after:bg-white after:content-['']  "
+      ></div>
+      {showBurgerMenu && (
+        <div
+          onClick={() => setShowBurgerMenu(!showBurgerMenu)}
+          className="absolute  left-0 top-0 h-full w-full overflow-hidden bg-black bg-opacity-20"
+        >
+          <div
+            onClick={(e) => {
+              e.stopPropagation();
+            }}
+            className="h-full w-5/12 bg-[#1a1919] max-sm:w-7/12"
+          >
+            <Navigation
+              onClick={() => setShowBurgerMenu(!showBurgerMenu)}
+              styleClassFlex={
+                "flex-col flex space-x-0 space-y-20 text-xl items-center pt-7 max-sm:text-lg"
+              }
+            />
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -3,22 +3,34 @@ import PropTypes from "prop-types";
 
 const linkStyles = "transition-all hover:text-[#D87D4A]";
 
-export const Navigation = ({ styleClassHidden, styleClassFlex }) => {
+export const Navigation = ({ styleClassHidden, styleClassFlex, onClick }) => {
   return (
     <nav className={`${styleClassHidden}`}>
       <ul
         className={`bold space-x-9 text-sm font-bold uppercase tracking-[3px] text-white ${styleClassFlex}`}
       >
-        <Link to="/" className={linkStyles}>
+        <Link to="/" onClick={onClick} className={linkStyles}>
           Home
         </Link>
-        <Link to="/products?category=headphones" className={linkStyles}>
+        <Link
+          to="/products?category=headphones"
+          onClick={onClick}
+          className={linkStyles}
+        >
           Headphones
         </Link>
-        <Link to="/products?category=speakers" className={linkStyles}>
+        <Link
+          to="/products?category=speakers"
+          onClick={onClick}
+          className={linkStyles}
+        >
           Speakers
         </Link>
-        <Link to="/products?category=earphones" className={linkStyles}>
+        <Link
+          to="/products?category=earphones"
+          onClick={onClick}
+          className={linkStyles}
+        >
           Earphones
         </Link>
       </ul>
@@ -29,4 +41,5 @@ export const Navigation = ({ styleClassHidden, styleClassFlex }) => {
 Navigation.propTypes = {
   styleClassHidden: PropTypes.string,
   styleClassFlex: PropTypes.string,
+  onClick: PropTypes.func,
 };
